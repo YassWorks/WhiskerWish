163fc3d71e765272d0128e3dc2918ee3be941446
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2025-04-17 01:28:59 +0100

    minor fixes

--- app/(root)/adopt/[id]/page.tsx
+++ app/(root)/adopt/[id]/page.tsx
@@ -2,38 +2,30 @@ import React from "react";
 import { auth, signIn } from "@/auth";
 import Animal from "@/app/components/Animal";
 
-type Props = {
-    data: {
-        id: string;
-    };
-};
-
-const AnimalPage = async ({ data }: Props) => {
+const AnimalPage = async ({ params }: { params: { id: string } }) => {
     const session = await auth();
-    const { id } = data;
+    const { id } = params;
+
     return (
         <>
-            {session && session?.user ? (
-                <>
-                    <div className="flex flex-col items-center justify-center h-screen bg-orange-100 text-gray-800">
-                        <Animal id={id} />
-                    </div>
-                </>
+            {session && session.user ? (
+                <div className="flex flex-col items-center justify-center min-h-screen bg-orange-100 text-gray-800">
+                    <Animal id={id} />
+                </div>
             ) : (
-                <div className="flex flex-col items-center justify-center h-screen bg-orange-100 text-gray-800">
-                    <p className="text-4xl text-center">
-                        Please login to browse the pets available for adoption.
+                <div className="flex flex-col items-center justify-center min-h-screen bg-orange-100 text-gray-800 text-center px-4">
+                    <p className="text-3xl md:text-4xl">
+                        Please log in to browse our adoptable buddies!
                     </p>
                     <form
                         action={async () => {
                             "use server";
-
                             await signIn("github");
                         }}
                     >
                         <button
                             type="submit"
-                            className="mt-4 bg-gradient-to-br from-orange-300 to-orange-200 text-white px-4 py-2 rounded hover:text-orange-800 cursor-pointer text-2xl"
+                            className="mt-6 bg-gradient-to-br from-orange-300 to-orange-200 text-white px-6 py-3 rounded-xl hover:text-orange-800 text-xl transition"
                         >
                             Login
                         </button>
